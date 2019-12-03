113d97ab91787662e3b3b6b73e4d5ece606d2bb6
NorthonFm <northon.melo@semptcl.com.br>
2019-12-03 16:48:02 -0400

    MyMoneyApp - Criação de componentes com Redux

---
+++ frontend/src/common/tab/tabActions.js
@@ -0,0 +1,16 @@
+export const selectTab = tabId =>{
+    console.log(tabId)
+    return{
+        type: 'TAB_SELECTED',
+        payload: tabId
+    }
+}
+
+export const showTabs = (...tabIds) => {
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return{
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
