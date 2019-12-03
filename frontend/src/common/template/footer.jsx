fab15bf6829c1aec0e972788a94eb77b972e5bc5
northfar <north.farias@gmail.com>
2019-12-02 22:36:35 -0400

    Frontend da Aplicação My Money App

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019 &ensp;
+            <a href="https://github.com/northfar" target="_blank" rel="noopener noreferrer">
+                Northon Farias
+            </a>
+        </strong>
+    </footer>
+)
