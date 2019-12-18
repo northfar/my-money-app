c273adc7b2e775c45c4b4295ca78900e3744cd4d
= <=>
2019-12-17 20:44:26 -0400

    MyMoneyApp - exclusão de linha de console

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -7,7 +7,6 @@ module.exports = (req, res, next) => {
     }
     else{
         const token = req.body.token || req.query.token || req.headers['authorization']
-        console.log(req.headers['authorization'])
         if(!token){
            return res.status(403).send({errors: ['No token provided']})
         }
